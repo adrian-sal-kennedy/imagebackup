ad002cee7c135cfd43cc92578889c94f6a7df109
adrian-sal-kennedy <adrian.sal.kennedy@gmail.com>
2020-03-11 10:29:27 +1100

    move everything into methods

--- lib/imagebackup.rb
+++ lib/imagebackup.rb
@@ -1,13 +1,21 @@
 #!/usr/bin/env ruby
 require_relative 'imagebackup/version'
 
+# require_relative ''
+
 require 'exiv2'
 require 'fileutils'
 require 'ffprober'
 require 'json'
 
 def file_types_list
-  puts "TODO: a method to create an ugly Dir.glob string from a readable list of file types that can be pushed from CLI"
+  return [
+    '**/*.CR2',
+    '**/*.DNG',
+    '**/*.MOV',
+    '**/*.MP4',
+    '**/*.JPG'
+  ]
 end
 
 def get_dates(file)
@@ -23,30 +31,26 @@ def get_dates(file)
   return date
 end
 
-dest = ARGV[0]
+# dest = ARGV[0]
 
-file_types = [
-  '**/*.CR2',
-  '**/*.DNG',
-  '**/*.MOV',
-  '**/*.MP4',
-  '**/*.JPG'
-]
+def main_loop(dest)
+  file_types = file_types_list()
+  Dir.glob(file_types).reverse_each do |f|
 
-Dir.glob(file_types).reverse_each do |f|
+    file = "#{Dir.pwd}/#{f}"
 
-  file = "#{Dir.pwd}/#{f}"
-  
-  date = get_dates(file)
-  destpath = "#{dest}/#{date}"
-  destpath = destpath.gsub('//','/')
-  outfile =  "#{destpath}/#{File.basename(file)}"
-  if File.exist?(outfile)
-    puts "\"#{outfile}\" already exists."
-  else
-    puts "copying \"#{file}\" to \"#{outfile}\""
-    FileUtils.mkdir_p(destpath)
-    FileUtils.cp(file,outfile)
+    date = get_dates(file)
+    destpath = "#{dest}/#{date}"
+    destpath = destpath.gsub('//','/')
+    outfile =  "#{destpath}/#{File.basename(file)}"
+    if File.exist?(outfile)
+      puts "\"#{outfile}\" already exists."
+    else
+      puts "copying \"#{file}\" to \"#{outfile}\""
+      puts "FileUtils.mkdir_p(#{destpath})"
+      puts "FileUtils.cp(#{file},#{outfile})"
+    end
   end
-  # sleep 0.1
 end
+
+main_loop(ARGV[0])
